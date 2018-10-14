dfe0de498d53b1ddbbb97ebd504f3c2361e4798e
Chris O'Connell <chris@chrisoconnell.info>
2018-10-14 16:16:04 -0700

    Add README.md file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Skeleton Ionic Firebase app
+
+This is a good starting point for Ionic 4 apps that have a Firebase backend.
+
+You will need to insert your Firebase credentials into `src/environments/environment.ts` and `src/environments/environment.prod.ts`
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyC0sijjsj8JHw_iGP2DxmVCfGOgO5oJXcE',
-    authDomain: 'fireslack-b2bdb.firebaseapp.com',
-    databaseURL: 'https://fireslack-b2bdb.firebaseio.com',
-    projectId: 'fireslack-b2bdb',
-    storageBucket: 'fireslack-b2bdb.appspot.com',
-    messagingSenderId: '83716123463'
+    apiKey: 'Your Firebase Credentials Here',
+    authDomain: 'Your Firebase Credentials Here',
+    databaseURL: 'Your Firebase Credentials Here',
+    projectId: 'Your Firebase Credentials Here',
+    storageBucket: 'Your Firebase Credentials Here',
+    messagingSenderId: 'Your Firebase Credentials Here'
   }
 };
